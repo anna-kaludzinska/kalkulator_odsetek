00c9d5021b2f68a7fdaac76afc7e32289d692cd5
Anna Kaludzinska <anna.kaludzinska@gmail.com>
2016-12-10 14:04:41 +0100

    last changes

--- js/main.js
+++ js/main.js
@@ -3,31 +3,29 @@
 document.getElementById("btn").addEventListener("click", calculateInterests);
 
 function calculateInterests() {
-	
+
 	var ko = document.getElementById("wplata").value;
-	var r = document.getElementById("stopa").value/100;
-	
+	var r = document.getElementById("stopa").value / 100;
+
 	var e = document.getElementById("kapitalizacja");
 	var m = e.options[e.selectedIndex].value;
-	
-	var n = document.getElementById("lata").value;
-	
-	var potega = n * m;
-	
-	var wynik1 = 1+(r/m);
-	
-	var wynik2 = Math.pow(wynik1, potega);
-	
-	var wynik3 = ko * wynik2;
-	
-	if (document.getElementById("podatek").checked){
-		var podatekBelki = wynik3 - (wynik3*0.19);
-		console.log(podatekBelki);
-	} else {
-		console.log(wynik3);
-	}
-	
-	
-}
 
-//calculateInterests();
+	var n = document.getElementById("lata").value;
+
+	var potega = n * m;
+
+	var wynik1 = 1 + (r / m);
+
+	var wynik2 = Math.pow(wynik1, potega);
+
+	var wynik3 = ko * wynik2;
+
+	if (document.getElementById("podatek").checked) {
+		var podatekBelki = (wynik3 - ((wynik3 - ko) * 0.19));
+		var wynikBelka = parseFloat(podatekBelki).toFixed(2);
+		document.getElementById("wynik").innerHTML = wynikBelka;
+	} else {
+		var wynikBezBelki = parseFloat(wynik3).toFixed(2);
+		document.getElementById("wynik").innerHTML = wynikBezBelki;
+	}
+}
